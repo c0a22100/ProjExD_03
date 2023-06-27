90df9d1f0c4e30d9a807830c3cd919ebd9a8260a
ymf65 <c0a221001d@edu.teu.ac.jp>
2023-06-27 17:32:39 +0900

    追加機能５：完了

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -41,14 +41,19 @@ class Bird:
         引数1 num：こうかとん画像ファイル名の番号
         引数2 xy：こうかとん画像の位置座標タプル
         """
-        self.img = pg.transform.flip(  # 左右反転
-            pg.transform.rotozoom(  # 2倍に拡大
-                pg.image.load(f"ex03/fig/{num}.png"), 
-                0, 
-                2.0), 
-            True, 
-            False
-        )
+        img0 = pg.transform.rotozoom(pg.image.load(f"ex03/fig/{num}.png"), 0, 2.0)  # 左向き
+        img = pg.transform.flip(img0, True, False)  # 右向き
+        self.imgs = {
+            (+5, 0): img,  # 右
+            (+5, -5): pg.transform.rotozoom(img, 45, 1.0),  # 右上
+            (0, -5): pg.transform.rotozoom(img, 90, 1.0),  # 上
+            (-5, -5): pg.transform.rotozoom(img0, -45, 1.0),  # 左上
+            (-5, 0): img0,  # 左
+            (-5, +5): pg.transform.rotozoom(img0, 45, 1.0),  # 左下
+            (0, +5): pg.transform.rotozoom(img, -90, 1.0),  # 下
+            (+5, +5): pg.transform.rotozoom(img, -45, 1.0),  # 右下
+        }
+        self.img = self.imgs[(+5, 0)]  # デフォルト：右向き
         self.rct = self.img.get_rect()
         self.rct.center = xy
 
@@ -75,6 +80,8 @@ class Bird:
         self.rct.move_ip(sum_mv)
         if check_bound(self.rct) != (True, True):
             self.rct.move_ip(-sum_mv[0], -sum_mv[1])
+        if not (sum_mv[0] == 0 and sum_mv[1] == 0):  # 何かしらの矢印キーが押されていたら
+            self.img = self.imgs[tuple(sum_mv)] 
         screen.blit(self.img, self.rct)
 
 
@@ -123,14 +130,29 @@ class Beam:
         self.rct.left = bird.rct.right
         self.rct.centery = bird.rct.centery
         self.vx, self.vy = +5, 0
-    
+
     def update(self, screen: pg.Surface):
         """
         ビームを速度ベクトルself.vx, self.vyに基づき移動させる
         引数 screen：画面Surface
         """
-        self.rct.move_ip(self.vx, self.vy)
+        #追加機能５（独自の機能）
+        self.rct.move_ip(self.vx, self.vy+random.randint(-10,10))
         screen.blit(self.img, self.rct)
+
+
+# 追加課題1（爆発エフェクト）
+class Explosion:
+    def __init__(self, life):
+        self.img_xpl = [
+            pg.transform.flip(pg.image.load("ex03/fig/explosion.gif"), True, True),
+            pg.transform.flip(pg.image.load("ex03/fig/explosion.gif"), True, False),
+            pg.transform.flip(pg.image.load("ex03/fig/explosion.gif"), False, True),
+            pg.transform.flip(pg.image.load("ex03/fig/explosion.gif"), False, False),
+        ]
+        Bomb.rct.move_ip(self.vx, self.vy)
+        pg.Surface.blit(self.img_xpl[1], )
+        self.life = life        
 
 
 def main():
@@ -166,6 +188,8 @@ def main():
                 if bomb.rct.colliderect(beam.rct):
                     bombs[i] = None
                     beam = None
+                    # 追加課題1
+                    explosion = Explosion(5)
                     bird.change_img(6, screen)
                     pg.display.update()              
 
